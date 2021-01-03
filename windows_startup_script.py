cabc143ad636caec28f7949325af167c4d993492
lucifeermorningstar <76868561+lucifeermorningstar@users.noreply.github.com>
2021-01-03 12:56:10 +0530

    Update windows_startup_script.py

--- windows_startup_script.py
+++ windows_startup_script.py
@@ -1,8 +1,8 @@
 from telethon import TelegramClient
-API_KEY="Bunu buraya yazın"
-API_HASH="Bunu buraya yazın"
-#my.telegram.org adresinden alın
+API_KEY="Write this here"
+API_HASH="Write this here"
+#my.telegram.org get it from
 bot = TelegramClient('userbot',API_KEY,API_HASH)
 bot.start()
 
-#Bu komut dosyası botunuzu çalıştırmaz, sadece bir oturum oluşturur.
+#This script won't run your bot, it just creates a session.
